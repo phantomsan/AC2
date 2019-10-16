beddf6e72d26cfa31aee4f12d0d436278d5604ec
phantomsan <ricardoiva9@gmail.com>
2019-10-16 20:47:15 -0300

    AC2 - API6

---
+++ src/main/java/br/com/drummond/Ac21Application.java
@@ -0,0 +1,13 @@
+package br.com.drummond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ac21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ac21Application.class, args);
+	}
+
+}
