e0af3b86219450f5b9ec5c002c54eb547336bd11
phantomsan <ricardoiva9@gmail.com>
2019-10-30 22:34:44 -0200

    commitado

---
+++ teste.vue
@@ -0,0 +1,12 @@
+new Vue({
+  el: '#app',
+  data: {
+    number1: 0,
+    number2: 0,
+  },
+  computed: {
+  	result() {
+    	return parseInt(this.number1) + parseInt(this.number2);
+    }
+  }
+});
